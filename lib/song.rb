bd1d006738d107512737b5fa2d7bac3f5a936160
Ben Spotswood <spotswoodb@gmail.com>
2020-12-01 19:48:26 -0600

    commit tree 0602765fe2bb6c1429cf2d5852c848e171421c05

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,11 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+  # def initialize
+  #   @save = save
+  # end
+
+
   def self.all
     @@all
   end
@@ -9,5 +14,60 @@ class Song
   def save
     self.class.all << self
   end
+
+  def self.create
+    song = self.new
+    @@all << song
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.create
+    song.name = name
+    song
+  end
+
+  def self.find_by_name(name)
+    self.all.find {|title| title.name == name} 
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
+  end
+
+  def self.alphabetical
+    self.all.sort_by{|letter| letter.name}
+  end
+
+  def self.new_from_filename(file_name)
+    title = file_name.split(" - ")
+    artist = title[0]
+    song_title = title[1].gsub(".mp3", "")
+    song = self.new
+    song.name = song_title
+    song.artist_name = artist
+    song
+  end
+
+  def self.create_from_filename(file_name)
+    title = file_name.split(" - ")
+    artist = title[0]
+    song_title = title[1].gsub(".mp3", "")
+    song = self.create
+    song.name = song_title
+    song.artist_name = artist
+    song
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
 
 end
